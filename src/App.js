4475a39cda4f46d93fcd2b59456cb16e40a4efc9
Ilham Yusuf <ilham.0992@students.amikom.ac.id>
2022-07-19 06:45:18 +0700

    [FIX] confilct app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+<<<<<<< HEAD
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./Pages/Home/Home";
@@ -22,6 +23,22 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
+=======
+import Login from "./Pages/Login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </>
+>>>>>>> login
   );
 }
 
