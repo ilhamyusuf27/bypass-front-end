55e5bb6373522805500984902e99a8e951676364
Ilham Yusuf <ilham.0992@students.amikom.ac.id>
2022-07-19 19:53:36 +0700

    [FIX] Ready to merge with staging

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Login from "./Pages/Login/Login";
 import Home from "./Pages/Home/Home";
+import ProfileCompany from "./Pages/ProfileCompany/ProfileCompany";
 import WithoutNavFooter from "./Components/NavFooterWrapper/WithoutNavFooter";
 import WithNavFooter from "./Components/NavFooterWrapper/WithNavFooter";
 import NotFound from "./Pages/NotFound/NotFound";
@@ -13,14 +14,17 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				{/* page without navbar and footer */}
+				{/* page with no navbar and footer */}
 				<Route element={<WithoutNavFooter />}>
+					<Route path="login" element={<Login />} />
+					<Route path="register" element={<Login />} />
 					<Route path="*" element={<NotFound />} />
 					<Route path="login" element={<Login />} />
 				</Route>
 
 				{/* page with navbar and footer */}
 				<Route element={<WithNavFooter />}>
+					<Route path="/profile-company" element={<ProfileCompany />} />
 					<Route path="/home" element={<Home />} />
 				</Route>
 			</Routes>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -10,210 +10,145 @@ import { GoLocation } from "react-icons/go";
 import profileImg from "../../Assets/Images/profile image example.jpeg";
 
 const Home = () => {
-  return (
-    <>
-      <div className="row">
-        <h3 className="header-home py-3 wrapper-home">Top Jobs</h3>
-      </div>
+	return (
+		<>
+			<div className="row">
+				<h3 className="header-home py-3 wrapper-home">Top Jobs</h3>
+			</div>
 
-      <div className="wrapper-home mt-3">
-        <InputGroup className="mb-3">
-          <Form.Control
-            aria-label="Text input with dropdown button"
-            size="lg"
-          />
-          <DropdownButton
-            variant="light"
-            title="Kategori"
-            id="input-group-dropdown-2"
-            align="end"
-          >
-            <Dropdown.Item href="#">Sortir Berdasarkan Nama</Dropdown.Item>
-            <Dropdown.Item href="#">Sortir Berdasarkan Skill</Dropdown.Item>
-            <Dropdown.Item href="#">Sortir Berdasarkan Lokasi</Dropdown.Item>
-            <Dropdown.Item href="#">Sortir Berdasarkan Freelance</Dropdown.Item>
-            <Dropdown.Item href="#">Sortir Berdasarkan Fulltime</Dropdown.Item>
-          </DropdownButton>
-          <Button variant="flat">Search</Button>
-        </InputGroup>
-        <div className="row">
-          <div className="col-md-5">
-            <div
-              className="card mb-3"
-              style={{ maxWidth: "540px", border: "none" }}
-            >
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <div>
-                    <img
-                      src={profileImg}
-                      className="img-fluid rounded-start img-user-home"
-                      alt="..."
-                    />
-                  </div>
-                </div>
-                <div className="col-md-8 align-self-center">
-                  <div className="card-body body-user-home">
-                    <h5 className="card-title">Louis Tomlinson</h5>
-                    <p className="card-text">Web Developer</p>
-                    <p className="card-text">
-                      <small className="text-muted">
-                        <GoLocation /> lorem ipsum
-                      </small>
-                    </p>
-                    <span className="skills-badge-home">
-                      <Badge bg="warning">PHP</Badge>
-                    </span>
-                    <span className="skills-badge-home">
-                      <Badge bg="warning">JavaScript</Badge>
-                    </span>
-                    <span className="skills-badge-home">
-                      <Badge bg="warning">HTML</Badge>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-2 offset-md-5 align-self-center text-end">
-            <Button variant="flat">Lihat Profile</Button>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-md-5">
-            <div
-              className="card mb-3"
-              style={{ maxWidth: "540px", border: "none" }}
-            >
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <div>
-                    <img
-                      src={profileImg}
-                      className="img-fluid rounded-start img-user-home"
-                      alt="..."
-                    />
-                  </div>
-                </div>
-                <div className="col-md-8 align-self-center">
-                  <div className="card-body body-user-home">
-                    <h5 className="card-title">Louis Tomlinson</h5>
-                    <p className="card-text">Web Developer</p>
-                    <p className="card-text">
-                      <small className="text-muted">
-                        <GoLocation /> lorem ipsum
-                      </small>
-                    </p>
-                    <span className="skills-badge-home">
-                      <Badge bg="warning">PHP</Badge>
-                    </span>
-                    <span className="skills-badge-home">
-                      <Badge bg="warning">JavaScript</Badge>
-                    </span>
-                    <span className="skills-badge-home">
-                      <Badge bg="warning">HTML</Badge>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-2 offset-md-5 align-self-center text-end">
-            <Button variant="flat">Lihat Profile</Button>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-md-5">
-            <div
-              className="card mb-3"
-              style={{ maxWidth: "540px", border: "none" }}
-            >
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <div>
-                    <img
-                      src={profileImg}
-                      className="img-fluid rounded-start img-user-home"
-                      alt="..."
-                    />
-                  </div>
-                </div>
-                <div className="col-md-8 align-self-center">
-                  <div className="card-body body-user-home">
-                    <h5 className="card-title">Louis Tomlinson</h5>
-                    <p className="card-text">Web Developer</p>
-                    <p className="card-text">
-                      <small className="text-muted">
-                        <GoLocation /> lorem ipsum
-                      </small>
-                    </p>
-                    <span className="skills-badge-home">
-                      <Badge bg="warning">PHP</Badge>
-                    </span>
-                    <span className="skills-badge-home">
-                      <Badge bg="warning">JavaScript</Badge>
-                    </span>
-                    <span className="skills-badge-home">
-                      <Badge bg="warning">HTML</Badge>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-2 offset-md-5 align-self-center text-end">
-            <Button variant="flat">Lihat Profile</Button>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-md-5">
-            <div
-              className="card mb-3"
-              style={{ maxWidth: "540px", border: "none" }}
-            >
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <div>
-                    <img
-                      src={profileImg}
-                      className="img-fluid rounded-start img-user-home"
-                      alt="..."
-                    />
-                  </div>
-                </div>
-                <div className="col-md-8 align-self-center">
-                  <div className="card-body body-user-home">
-                    <h5 className="card-title">Louis Tomlinson</h5>
-                    <p className="card-text">Web Developer</p>
-                    <p className="card-text">
-                      <small className="text-muted">
-                        <GoLocation /> lorem ipsum
-                      </small>
-                    </p>
-                    <span className="skills-badge-home">
-                      <Badge bg="warning">PHP</Badge>
-                    </span>
-                    <span className="skills-badge-home">
-                      <Badge bg="warning">JavaScript</Badge>
-                    </span>
-                    <span className="skills-badge-home">
-                      <Badge bg="warning">HTML</Badge>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-2 offset-md-5 align-self-center text-end">
-            <Button variant="flat">Lihat Profile</Button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+			<div className="wrapper-home mt-3">
+				<InputGroup className="mb-3">
+					<Form.Control aria-label="Text input with dropdown button" size="lg" />
+					<DropdownButton variant="light" title="Kategori" id="input-group-dropdown-2" align="end">
+						<Dropdown.Item href="#">Sortir Berdasarkan Nama</Dropdown.Item>
+						<Dropdown.Item href="#">Sortir Berdasarkan Skill</Dropdown.Item>
+						<Dropdown.Item href="#">Sortir Berdasarkan Lokasi</Dropdown.Item>
+						<Dropdown.Item href="#">Sortir Berdasarkan Freelance</Dropdown.Item>
+						<Dropdown.Item href="#">Sortir Berdasarkan Fulltime</Dropdown.Item>
+					</DropdownButton>
+					<Button variant="flat">Search</Button>
+				</InputGroup>
+				<div className="card mb-3" style={{ maxWidth: "100%", border: "none" }}>
+					<div className="row">
+						<div className="col-md-5">
+							<div className="card mb-3" style={{ maxWidth: "540px", border: "none" }}>
+								<div className="row g-0">
+									<div className="col-md-4">
+										<div>
+											<img src={profileImg} className="img-fluid rounded-start img-user-home" alt="..." />
+										</div>
+									</div>
+									<div className="col-md-8 align-self-center">
+										<div className="card-body body-user-home">
+											<h5 className="card-title">Louis Tomlinson</h5>
+											<p className="card-text">Web Developer</p>
+											<p className="card-text">
+												<small className="text-muted">
+													<GoLocation /> lorem ipsum
+												</small>
+											</p>
+											<span className="skills-badge-home">
+												<Badge bg="warning">PHP</Badge>
+											</span>
+											<span className="skills-badge-home">
+												<Badge bg="warning">JavaScript</Badge>
+											</span>
+											<span className="skills-badge-home">
+												<Badge bg="warning">HTML</Badge>
+											</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-7 d-flex justify-content-end align-items-center">
+							<div className="col-md-2 ">
+								<Button variant="flat">Lihat Profile</Button>
+							</div>
+						</div>
+					</div>
+					<hr />
+					<div className="row">
+						<div className="col-md-5">
+							<div className="card mb-3" style={{ maxWidth: "540px", border: "none" }}>
+								<div className="row g-0">
+									<div className="col-md-4">
+										<div>
+											<img src={profileImg} className="img-fluid rounded-start img-user-home" alt="..." />
+										</div>
+									</div>
+									<div className="col-md-8 align-self-center">
+										<div className="card-body body-user-home">
+											<h5 className="card-title">Louis Tomlinson</h5>
+											<p className="card-text">Web Developer</p>
+											<p className="card-text">
+												<small className="text-muted">
+													<GoLocation /> lorem ipsum
+												</small>
+											</p>
+											<span className="skills-badge-home">
+												<Badge bg="warning">PHP</Badge>
+											</span>
+											<span className="skills-badge-home">
+												<Badge bg="warning">JavaScript</Badge>
+											</span>
+											<span className="skills-badge-home">
+												<Badge bg="warning">HTML</Badge>
+											</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-7 d-flex justify-content-end align-items-center">
+							<div className="col-md-2 ">
+								<Button variant="flat">Lihat Profile</Button>
+							</div>
+						</div>
+					</div>
+					<hr />
+					<div className="row">
+						<div className="col-md-5">
+							<div className="card mb-3" style={{ maxWidth: "540px", border: "none" }}>
+								<div className="row g-0">
+									<div className="col-md-4">
+										<div>
+											<img src={profileImg} className="img-fluid rounded-start img-user-home" alt="..." />
+										</div>
+									</div>
+									<div className="col-md-8 align-self-center">
+										<div className="card-body body-user-home">
+											<h5 className="card-title">Louis Tomlinson</h5>
+											<p className="card-text">Web Developer</p>
+											<p className="card-text">
+												<small className="text-muted">
+													<GoLocation /> lorem ipsum
+												</small>
+											</p>
+											<span className="skills-badge-home">
+												<Badge bg="warning">PHP</Badge>
+											</span>
+											<span className="skills-badge-home">
+												<Badge bg="warning">JavaScript</Badge>
+											</span>
+											<span className="skills-badge-home">
+												<Badge bg="warning">HTML</Badge>
+											</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-7 d-flex justify-content-end align-items-center">
+							<div className="col-md-2 ">
+								<Button variant="flat">Lihat Profile</Button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Home;
