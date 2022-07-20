959ed7dec21297e9c6ba343317d4c63145dd9ed8
taufik17 <taufikagungsantoso17@gmail.com>
2022-07-20 13:06:28 +0700

    selesai tampilan chat kosong

--- src/Pages/Chat/Chat.js
+++ src/Pages/Chat/Chat.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import {
+  Container,
+  Row,
+  Button,
+  Card,
+  Form,
+  FloatingLabel,
+} from "react-bootstrap";
+
+import EmptyChat from "../../Assets/Images/empty-chat.png";
+import { BiMap } from "react-icons/bi";
+
+import "./Chat.css";
 
 function Chat() {
   return (
-    <div>Chat</div>
-  )
+    // ini isi
+
+    // ini kosong
+    <>
+      <Container className="mb-5">
+        <Row>
+          <div className="col-lg-4 col-md-4 col-sm-12">
+            <Card className="card mt-5 empty-image">
+                <img src={EmptyChat} alt="" />
+              <div className="info m-3">
+                <div className="info">
+                  <h4>Belum ada chat</h4>
+                </div>
+              </div>
+            </Card>
+          </div>
+          <div className="col-lg-8 col-md-8 col-sm-12">
+            <Card className="mt-5">
+              <Card.Header as="h5" className="empty-header"></Card.Header>
+              <Card.Body></Card.Body>
+            </Card>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default Chat
+export default Chat;
