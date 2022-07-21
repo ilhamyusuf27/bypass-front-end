fa8cd603b14e2ac83f13da9df52b2262b2035f12
Ilham Yusuf <ilham.0992@students.amikom.ac.id>
2022-07-21 18:50:35 +0700

    [ADD] Landing Page

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -4,8 +4,6 @@ import { Container } from "react-bootstrap";
 
 import LandingPage2 from "../../Components/LandingPage2/LandingPage2";
 
-import "./LandingPage.css";
-
 function LandingPage() {
 	return (
 		<>
