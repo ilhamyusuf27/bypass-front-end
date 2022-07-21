6c00e0390547f60d0e726b26c88e8dec0fcd6ca7
rezcode <rezharians@gmail.com>
2022-07-21 23:25:23 +0800

    add banner (landing page 1) and navbar login button

---
+++ src/Components/LandingPage1/LandingPage1.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./LandingPage1.css";
+import { Container, Button } from "react-bootstrap";
+import imgBanner from "../../Assets/Images/banner.png";
+import { Link } from "react-router-dom";
+
+const LandingPage1 = () => {
+  return (
+    <>
+      <div className="banner-bg">
+        <Container>
+          <div className="row align-items-center">
+            <div className="col-6">
+              <div className="title-banner">
+                <h2>Talenta terbaik negri untuk perubahan revolusi 4.0</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                  euismod ipsum et dui rhoncus auctor.
+                </p>
+                <Link to="/register-company">
+                  <Button variant="flat" size="lg">
+                    Mulai Dari Sekarang
+                  </Button>
+                </Link>
+              </div>
+            </div>
+            <div className="col-6">
+              <img src={imgBanner} alt="img-banner" />
+            </div>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage1;
