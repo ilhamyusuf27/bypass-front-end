d6b0ca1eb25ad0ba183bbc30a8c524e994996574
taufik17 <taufikagungsantoso17@gmail.com>
2022-07-22 00:57:43 +0700

    landing 3

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -7,15 +7,9 @@ import LandingPage3 from "../../Components/LandingPage3/LandingPage3";
 const LandingPage = () => {
 	return (
 		<>
-<<<<<<< HEAD
-			<Container>
-				<LandingPage2 />
-				<LandingPage3 />
-			</Container>
-=======
 			<LandingPage1 />
 			<LandingPage2 />
->>>>>>> origin/staging
+			<LandingPage3 />
 		</>
 	);
 };
