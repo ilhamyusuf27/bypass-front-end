92134ca8a96ee952bf16d8fc5eea319c5d503963
taufik17 <taufikagungsantoso17@gmail.com>
2022-07-21 22:31:49 +0700

    first

---
+++ src/Components/LandingPage3/LandingPage3.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Col, Row, Button, Container } from "react-bootstrap";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import "../../Pages/LandingPage/LandingPage.css";
+
+
+function LandingPage2() {
+	return (
+		<>
+		<h2>
+			Tes Hesoyam
+		</h2>
+		</>
+	);
+}
+
+export default LandingPage2;
--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -3,12 +3,14 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 import LandingPage2 from "../../Components/LandingPage2/LandingPage2";
+import LandingPage3 from "../../Components/LandingPage3/LandingPage3";
 
 function LandingPage() {
 	return (
 		<>
 			<Container>
 				<LandingPage2 />
+				<LandingPage3 />
 			</Container>
 		</>
 	);
