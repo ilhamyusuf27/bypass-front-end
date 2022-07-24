ebf964008d8f091e5f0060e5df423c8dd9cdfb5d
Ilham Yusuf <ilham.0992@students.amikom.ac.id>
2022-07-25 03:03:21 +0700

    Menambah yang kurang

--- src/Pages/Login/CompanyLogin.js
+++ src/Pages/Login/CompanyLogin.js
@@ -3,8 +3,15 @@ import { Container, Row } from "react-bootstrap";
 import "./Login.css";
 import LeftLogin from "../../Components/LeftLogin/LeftLogin";
 import RightLogin from "../../Components/RightLogin/RightLoginCompany";
+import { useNavigate } from "react-router";
 
 const CompanyLogin = () => {
+	const navigate = useNavigate();
+	React.useEffect(() => {
+		if (localStorage.getItem("token")) {
+			navigate("/");
+		}
+	}, []);
 	return (
 		<>
 			<Container fluid className="App">
