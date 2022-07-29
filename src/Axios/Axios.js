9058e2468fb78a7c5d0dd059dbc8057b6a9c048e
Ilham Yusuf <ilham.0992@students.amikom.ac.id>
2022-07-29 10:15:30 +0700

    [FIX] Hire page

--- src/Axios/Axios.js
+++ src/Axios/Axios.js
@@ -1,3 +1,8 @@
 import axios from "axios";
-const instance = axios.create({ baseURL: process.env.REACT_APP_URL_API });
+const instance = axios.create({
+	baseURL: process.env.REACT_APP_URL_API,
+	headers: {
+		Authorization: `Bearir ${localStorage.getItem("token")}`,
+	},
+});
 export default instance;
